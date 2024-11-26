84488fa106f3cf1ef602e490f6010a7fe972b262
samurai0320 <adhitya.wardhana0320@gmail.com>
2024-11-26 16:03:37 +0700

    tambahi bot nav bar di home page umkm

--- lib/widgets/UMKM/umkm_home_page.dart
+++ lib/widgets/UMKM/umkm_home_page.dart
@@ -1,22 +1,133 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class UMKMHomePage extends StatefulWidget {
-  const UMKMHomePage({super.key, required this.username, required this.password});
+  const UMKMHomePage({
+    super.key,
+    required this.username,
+    required this.password,
+  });
+
   final String username;
   final String password;
-  
+
   @override
   State<UMKMHomePage> createState() => _UMKMHomePageState();
 }
 
 class _UMKMHomePageState extends State<UMKMHomePage> {
+  int _currentIndex = 0; 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("UMKM Home Page ${widget.username} ${widget.password}"),
-        ),
+      body: ListView(
+        children: [
+          
+          Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 28, 125, 204),
+                  Color.fromARGB(255, 150, 213, 234),
+                ],
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+              ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    
+                    Container(
+                      width: 270,
+                      height: 40,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Search",
+                          hintStyle: TextStyle(
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          contentPadding: const EdgeInsets.only(
+                            left: 20,
+                            right: 20,
+                          ),
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(0.7),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    
+                    const Icon(
+                      Icons.notifications,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          
+          const SizedBox(height: 20),
+          Center(
+            child: Text(
+              "Welcome, ${widget.username}!",
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, 
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.window_sharp),
+            label: "Beranda",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cases_outlined),
+            label: "Kontrak",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group_rounded),
+            label: "Pekerja",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profil",
+          ),
+        ],
+        currentIndex: _currentIndex, 
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index; 
+          });
+        },
       ),
     );
   }
