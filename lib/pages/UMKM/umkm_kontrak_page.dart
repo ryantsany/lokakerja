6249435e56dd67c6509b974cae65d5c122af73b4
samurai0320 <adhitya.wardhana0320@gmail.com>
2024-12-11 01:17:33 +0700

    'nambah ini

--- lib/pages/UMKM/umkm_kontrak_page.dart
+++ lib/pages/UMKM/umkm_kontrak_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lokakerja/pages/UMKM/umkm_kontrak_page2.dart';
 import 'package:lokakerja/widgets/custom_top_bar.dart';
+import 'package:lokakerja/widgets/tab_button.dart';
+import 'package:flutter/widgets.dart';
 
 class KontrakPage extends StatelessWidget {
   @override
@@ -7,7 +10,75 @@ class KontrakPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          CustomTopBar(title: "Kontrak", icon: Icons.settings_rounded),
+          
+          CustomTopBar(title: "Kontrak", icon: Icons.person),
+
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TabButton(
+                text: "Buka Lowongan",
+                isActive: true,
+                onTap: () {},
+              ),
+              TabButton(
+                text: "Lowongan aktif",
+                isActive: false,
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => KontrakPage2()));
+                },
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 5),
+
+          
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Buka Lowongan\nKerja Baru",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  GestureDetector(
+                    onTap: () {
+                      
+                    },
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
