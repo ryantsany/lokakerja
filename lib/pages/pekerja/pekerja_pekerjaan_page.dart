754ec58ab7596b10fe153879cab6d32f8b4280fd
ryantsany <ryantsany2003@gmail.com>
2024-12-11 00:03:54 +0700

    Add PekerjaanPage layout with CustomTopBar and TabButton widget

--- lib/pages/pekerja/pekerja_pekerjaan_page.dart
+++ lib/pages/pekerja/pekerja_pekerjaan_page.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:lokakerja/widgets/currentjob.dart';
+import 'package:lokakerja/widgets/custom_top_bar.dart';
+import 'package:lokakerja/widgets/tab_button.dart';
 
 class PekerjaanPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("pekerjaan"),
-      ),
-      body: Center(
-        child: Text("Halaman pekerjaan", style: TextStyle(fontSize: 24)),
+      body: Column(
+        children: [
+          CustomTopBar(title: "Pekerjaan", icon: Icons.person),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TabButton(text: "Pekerjaan", isActive: true, onTap: (){}),
+              TabButton(text: "Kerjaan", isActive: false, onTap: (){}),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Padding(
+            child: Currentjob(),
+            padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+          ),
+          
+        ],
       ),
     );
   }
